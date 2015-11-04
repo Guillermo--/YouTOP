b531e1eb7ddffaab1c809761ba0de8c45439fd4e
Guillermo <gmo.chu92@gmail.com>
2015-11-03 22:09:03 -0500

    Created ng controller to get checked boxes

--- YouTOP/WebContent/js/app.js
+++ YouTOP/WebContent/js/app.js
@@ -1,1 +1,18 @@
 var app = angular.module('app', []);
+
+app.controller('checkboxController', function($scope) {
+
+	$scope.selectedItems = [];
+	
+	$scope.toggleSelection = function($event) {
+		var checkboxValue = $event.target.value;
+		var index = $scope.selectedItems.indexOf(checkboxValue);
+		
+		if (index > -1) {
+		    $scope.selectedItems.splice(index, 1);
+		}
+		else {
+		    $scope.selectedItems.push(checkboxValue);
+		}
+	}
+});
