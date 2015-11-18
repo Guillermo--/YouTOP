dc5a47f54d1a5a897969752af5fc2969eb1723dd
Guillermo <gmo.chu92@gmail.com>
2015-11-17 21:54:46 -0500

    Added spring config

--- YouTOP/src/main/config/WebConfig.java
+++ YouTOP/src/main/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("controllers")
+@ComponentScan("controller")
 public class WebConfig {
 
 }
