0467c268f8439fb3fb0d33c8eafe6edaa0871bd6
Guillermo <gmo.chu92@gmail.com>
2015-12-22 23:29:14 -0500

    redirect to dashboard.html only on login button click

--- YouTOP/WebContent/js/app.js
+++ YouTOP/WebContent/js/app.js
@@ -22,6 +22,9 @@ app.config(['$routeProvider', '$locationProvider', '$sceDelegateProvider', '$htt
 		template : " ",
 		controller : 'loginController'
 	})
+	.when('/dashboard', {
+		templateUrl : 'pages/userDashboard.html'
+	})
     .otherwise({
     	redirectTo : '/'
     });
@@ -231,18 +234,18 @@ app.controller('displayResultsController', function($scope, $rootScope){
 	});
 });
 
-app.controller('loginController', function($http, $scope, $rootScope, $window){
+app.controller('loginController', function(routeService, $http, $scope, $rootScope, $window, $location){
 	console.log("Initializing loginController");
 	
 	var clientId = '386721466748-hc6or76387qevpajh4iupstfsn11dc83.apps.googleusercontent.com';
 	var apiKey = 'AIzaSyDXz-f_jsDJZ8mF46OUjcNPjplnDtyeqeA';
-	var scopes = 'https://www.googleapis.com/auth/yt-analytics.readonly';
+	var scopes = ['https://www.googleapis.com/auth/yt-analytics.readonly', 'https://www.googleapis.com/auth/youtube.readonly'];
 
 	angular.element($window).bind('load', function() {
-		handleClientLoad();
+		handlePageLoad();
 	});
 	
-	function handleClientLoad() {
+	function handlePageLoad() {
 		gapi.client.setApiKey(apiKey);
 		window.setTimeout(checkAuth,1);
 		console.log("Client loaded...");
@@ -258,7 +261,6 @@ app.controller('loginController', function($http, $scope, $rootScope, $window){
       	if (authResult && !authResult.error) {
       		console.log("Auth successful!");
         	authorizeButton.style.visibility = 'hidden';
-        	//makeApiCall();
       	} else {
       		console.log("Auth result error...");
         	authorizeButton.style.visibility = '';
@@ -268,18 +270,66 @@ app.controller('loginController', function($http, $scope, $rootScope, $window){
     
     function handleAuthClick(event) {
     	console.log("Handling Auth Click");
-      	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult);
+      	gapi.auth.authorize({client_id: clientId, scope: scopes, immediate: false}, handleAuthResult)
+      		.then(function(){        	
+      			$window.location = './#/dashboard';
+      			console.log("redirect should've occurred")
+      		});
       	return false;
     }
     
+    
+    function loadAPIClientInterfaces() {
+        gapi.client.load('youtube', 'v3', function() {
+          gapi.client.load('youtubeAnalytics', 'v1', function() {
+            // After both client interfaces load, use the Data API to request
+            // information about the authenticated user's channel.
+            getUserChannel();
+          });
+        });
+      }
 
+    function getUserChannel() {
+    	
+        var request = gapi.client.youtube.channels.list({
+          // Setting the "mine" request parameter's value to "true" indicates that
+          // you want to retrieve the currently authenticated user's channel.
+          mine: true,
+          part: 'id,contentDetails'
+        });
+
+        request.execute(function(response) {
+          if ('error' in response) {
+            console.log(response.error.message);
+          } else {
+            // We need the channel's channel ID to make calls to the Analytics API.
+            // The channel ID value has the form "UCdLFeWKpkLhkguiMZUp8lWA".
+            channelId = response.items[0].id;
+            // Retrieve the playlist ID that uniquely identifies the playlist of
+            // videos uploaded to the authenticated user's channel. This value has
+            // the form "UUdLFeWKpkLhkguiMZUp8lWA".
+            var uploadsListId = response.items[0].contentDetails.relatedPlaylists.uploads;
+            // Use the playlist ID to retrieve the list of uploaded videos.
+            getPlaylistItems(uploadsListId);
+          }
+        });
+      }
+    
 });
 
 
-//------------------------- ROUTING FUNCTIONS ----------------------------
 
-app.controller('routeController', function($scope, $location){
+//------------------------- ROUTING FUNCTIONS ----------------------------
+app.service('routeService', function($location){
+	return{
+		go : function(path) {
+			$location.path(path);
+		}
+	}
+});
+
+app.controller('routeController', function(routeService, $scope){
 	$scope.go = function(path) {
-		$location.path(path);
+		routeService.go(path);
 	}
 });
